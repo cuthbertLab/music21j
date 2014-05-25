5898dda5e4a4c6b4f3fd67a38a1fe008f30dbb82
Michael Scott Cuthbert <cuthbert@mit.edu>
2014-05-25 13:17:50 -0400

    move ext (except require.js) into src.

--- src/loadMIDI.js
+++ src/loadMIDI.js
@@ -1,12 +1,12 @@
 if ( typeof define === "function" && define.amd) {
-	define(['../ext/require/order!../ext/midijs/js/MIDI/AudioDetect',
-            '../ext/require/order!../ext/midijs/js/MIDI/LoadPlugin',
-            '../ext/require/order!../ext/midijs/js/MIDI/Plugin',
-            '../ext/require/order!../ext/midijs/js/MIDI/Player',
-            '../ext/require/order!../ext/midijs/js/Window/DOMLoader.XMLHttp',
-            '../ext/require/order!../ext/midijs/js/Window/DOMLoader.script',                        
-            '../ext/require/order!../ext/midijs/inc/Base64',
-            '../ext/require/order!../ext/midijs/inc/base64binary',
+	define(['../ext/require/order!ext/midijs/js/MIDI/AudioDetect',
+            '../ext/require/order!ext/midijs/js/MIDI/LoadPlugin',
+            '../ext/require/order!ext/midijs/js/MIDI/Plugin',
+            '../ext/require/order!ext/midijs/js/MIDI/Player',
+            '../ext/require/order!ext/midijs/js/Window/DOMLoader.XMLHttp',
+            '../ext/require/order!ext/midijs/js/Window/DOMLoader.script',                        
+            '../ext/require/order!ext/midijs/inc/Base64',
+            '../ext/require/order!ext/midijs/inc/base64binary',
             'jquery'], 
            function(require) {
 	            $(document).ready( function() {
