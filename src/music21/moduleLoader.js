58732519dfc5282198e70953872559bcb8ce32b3
Michael Scott Cuthbert <cuthbert@mit.edu>
2014-05-29 15:16:52 -0400

    fix jqueryEventCopy

--- src/music21/moduleLoader.js
+++ src/music21/moduleLoader.js
@@ -37,37 +37,6 @@ music21.PYTHON_COMPAT_VERSION = [1, 9, 2]; // Python version built to match
 
 music21.debug = false;
 
-/* jQuery extensions
- * 
- */
-/**
- * Logic for copying events from one jQuery object to another.
- *
- * @private 
- * @name jQuery.event.copy
- * @param jQuery|String|DOM Element jQuery object to copy events from. Only uses the first matched element.
- * @param jQuery|String|DOM Element jQuery object to copy events to. Copies to all matched elements.
- * @type undefined
- * @cat Plugins/copyEvents
- * @author Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
- * @author Yannick Albert (mail@yckart.com || http://yckart.com)
- */
-if (typeof(jQuery) != "undefined") {
-	jQuery.event.copy = function (from, to) {
-	    from = from.jquery ? from : jQuery(from);
-	    to = to.jquery ? to : jQuery(to);
-	
-	    var events = from[0].events || jQuery.data(from[0], "events") || jQuery._data(from[0], "events");
-	    if (!from.length || !to.length || !events) return;
-	
-	    return to.each(function () {
-	        for (var type in events)
-	        for (var handler in events[type])
-	        jQuery.event.add(this, type, events[type][handler], events[type][handler].data);
-	    });
-	};
-}
-
 define(['music21/pitch', 'music21/duration', 'music21/base', 'music21/note', 'music21/meter',
         'music21/chord', 'music21/roman', 'music21/key', 'music21/interval', 'music21/clef',
         'music21/renderOptions', 'music21/tinyNotation', 'music21/dynamics', 'music21/stream',
