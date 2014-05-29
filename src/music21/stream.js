011af8fa154ea3720709273a48b8c39be5581745
Michael Scott Cuthbert <cuthbert@mit.edu>
2014-05-29 15:16:52 -0400

    fix jqueryEventCopy

--- src/music21/stream.js
+++ src/music21/stream.js
@@ -8,8 +8,8 @@
  * Based on music21 (=music21p), Copyright (c) 2006–14, Michael Scott Cuthbert and cuthbertLab
  * 
  */
-define(['music21/base','music21/renderOptions','music21/clef'], function(require) {
-	var stream = {};
+define(['music21/base','music21/renderOptions','music21/clef', 'jquery'], function(require) {   
+    var stream = {};
 	
 	stream.Stream = function () {
 		music21.base.Music21Object.call(this);
@@ -837,8 +837,8 @@ define(['music21/base','music21/renderOptions','music21/clef'], function(require
 												canvas.width,
 												canvas.height );
 			this.drawCanvas(newCanv[0]);
-			$(canvas).replaceWith( newCanv );
-			$.event.copy($(canvas), newCanv); /* copy events -- using custom extension... */
+			$(canvas).replaceWith( newCanv );		
+			stream.jQueryEventCopy($.event, $(canvas), newCanv); /* copy events -- using custom extension... */
 		};
 		
 		this.editableAccidentalCanvas = function (scaleInfo, width, height) {
@@ -1448,6 +1448,33 @@ define(['music21/base','music21/renderOptions','music21/clef'], function(require
 	stream.Score.prototype = new stream.Stream();
 	stream.Score.prototype.constructor = stream.Score;
 
+	/**
+	 * Logic for copying events from one jQuery object to another.
+	 *
+	 * @private 
+	 * @name jQuery.event.copy
+	 * @param jQuery|String|DOM Element jQuery object to copy events from. Only uses the first matched element.
+	 * @param jQuery|String|DOM Element jQuery object to copy events to. Copies to all matched elements.
+	 * @type undefined
+	 * @cat Plugins/copyEvents
+	 * @author Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
+	 * @author Yannick Albert (mail@yckart.com || http://yckart.com)
+	 */
+	stream.jQueryEventCopy = function  (eventObj, from, to) {
+        from = from.jquery ? from : jQuery(from);
+        to = to.jquery ? to : jQuery(to);
+    
+        var events = from[0].events || jQuery.data(from[0], "events") || jQuery._data(from[0], "events");
+        if (!from.length || !to.length || !events) return;
+    
+        return to.each(function () {
+            for (var type in events)
+                for (var handler in events[type])
+                    jQuery.event.add(eventObj, type, events[type][handler], events[type][handler].data);
+        });
+    };
+	
+	
 	stream.tests = function () {
 	    test( "music21.stream.Stream", function() {
 	        var s = new music21.stream.Stream();
