8cb2c03fe5ae0a46325088b28f93cf96165b36e0
Michael Scott Cuthbert <cuthbert@mit.edu>
2014-06-03 22:05:43 -0400

    followScore works for single part

--- src/music21/moduleLoader.js
+++ src/music21/moduleLoader.js
@@ -39,6 +39,7 @@ music21.debug = false;
 
 define(['music21/pitch', 
         'music21/duration', 
+        'music21/common',
         'music21/base', 
         'music21/note', 
         'music21/meter',
