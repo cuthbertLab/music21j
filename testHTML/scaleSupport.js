d40565850c002ff5eaf6330ec00d8225a5ce8f15
Michael Scott Cuthbert <cuthbert@ZACHARA.MIT.EDU>
2015-01-07 10:50:52 -0500

    node_modules .gitignore
    Former-commit-id: 10e5e2b8ba3e11f68e538f223410aea707d52ff2 [formerly ea287efe0b32725be3c7605a71526c5a54a90712 [formerly 7308687f4dc3deff6921fdbc071242d8a7b4e402]]
    Former-commit-id: 1a56dd458e16678818de9420bb3884645b43f743
    Former-commit-id: 7040be09906662e393f550b2eaa0f589c11b3fe3

---
+++ testHTML/scaleSupport.js
@@ -0,0 +1,67 @@
+require(['music21'], function() {
+	var scaleFunction = function() {
+		var k2 = document.getElementById('key').value;
+		console.log(k2);
+		k = new music21.key.Key(k2);
+		sc = k.getScale();
+		mainCanvas = $('#mainCanvas')[0];
+		s = new music21.stream.Stream();
+		s.replaceCanvas(mainCanvas);
+		s.append(k);
+		tonic = sc[0];
+		tonicString = tonic.name;
+		var Interval = function(n1, n2) {
+			this.n1 = n1;
+			this.n2 = n2;
+			this.size = n2.pitch.ps - n1.pitch.ps;
+			this.generic = function() {
+				if(this.n2.pitch.diatonicNoteNum > this.n1.pitch.diatonicNoteNum) {
+					var genericInterval = this.n2.pitch.diatonicNoteNum - this.n1.pitch.diatonicNoteNum + 1;
+				}
+				else if(this.n1.pitch.diatonicNoteNum > this.n2.pitch.diatonicNoteNum) {
+					var genericInterval = this.n2.pitch.diatonicNoteNum - this.n1.pitch.diatonicNoteNum - 1;
+				}
+				else if(this.n1.pitch.diatonicNoteNum === this.n2.pitch.diatonicNoteNum) {
+					var genericInterval = 1;
+				}
+				return genericInterval;
+			};
+		};
+		c5 = new music21.note.Note('C5');
+		tNote = new music21.note.Note(tonicString+'4');
+		intv = new Interval(tNote,c5);
+		genericInterval = intv.generic();
+		console.log(genericInterval);
+		
+		for (i=0 ; i < sc.length ; i++) {
+			if (i < genericInterval-1) {
+				p = sc[i];
+				pn = p.name;
+				n = new music21.note.Note(pn);
+				n.duration.type = "quarter";
+				s.append(n);
+				s.replaceCanvas();
+			}
+			else {
+				p = sc[i];
+				pn = p.name;
+				n = new music21.note.Note(pn+'5');
+				n.duration.type = "quarter";
+				s.append(n);
+				s.replaceCanvas();
+			}
+
+		};
+		
+	};
+	var playStream = function() {
+		s.playStream();
+	};
+	
+	
+	
+	playButton = $('#playButton');
+	playButton.click(playStream);
+	submit = $('#submit');
+	submit.click(scaleFunction);
+});
