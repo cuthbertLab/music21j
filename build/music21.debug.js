5acc29bf2c382ebfe73336a2dfa875e8f6cf9423
Michael Scott Cuthbert <cuthbert@mit.edu>
2017-06-14 14:12:31 -0400

    and another....

--- build/music21.debug.js
+++ build/music21.debug.js
@@ -1774,22 +1774,21 @@
        * This function which patches Safari requires some time to get started
        * so we call it on object creation.
        */
-
-      audioSearch.sampleBuffer = new Float32Array(audioSearch.fftSize / 2);
-      var mediaStreamSource = audioSearch.audioContext.createMediaStreamSource(audioStream);
-      var analyser = audioSearch.audioContext.createAnalyser();
-      if (!analyser.getFloatTimeDomainData) {
-          // Safari 11...
+      if (window.AnalyserNode && !window.AnalyserNode.prototype.getFloatTimeDomainData) {
           var uint8 = new Uint8Array(2048);
-          analyser.getFloatTimeDomainData = function getFloatTimeDomainData(array) {
+          var gftdd = function getFloatTimeDomainData(array) {
               this.getByteTimeDomainData(uint8);
               var imax = array.length;
               for (var i = 0; i < imax; i++) {
                   array[i] = (uint8[i] - 128) * 0.0078125;
               }
           };
+          window.AnalyserNode.prototype.getFloatTimeDomainData = gftdd;
       }
 
+      audioSearch.sampleBuffer = new Float32Array(audioSearch.fftSize / 2);
+      var mediaStreamSource = audioSearch.audioContext.createMediaStreamSource(audioStream);
+      var analyser = audioSearch.audioContext.createAnalyser();
       analyser.fftSize = audioSearch.fftSize;
       mediaStreamSource.connect(analyser);
       audioSearch.currentAnalyser = analyser;
