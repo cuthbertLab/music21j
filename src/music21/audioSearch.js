6e76aedf8b86fa095dd4b825168176d3a3ac9ce9
Michael Scott Cuthbert <cuthbert@mit.edu>
2017-06-14 14:09:38 -0400

    another try...

--- src/music21/audioSearch.js
+++ src/music21/audioSearch.js
@@ -91,21 +91,21 @@ audioSearch.userMediaStarted = function userMediaStarted(audioStream) {
      * This function which patches Safari requires some time to get started
      * so we call it on object creation.
      */
-    if (window.AnalyserNode && !window.AnalyserNode.prototype.getFloatTimeDomainData) {
+
+    audioSearch.sampleBuffer = new Float32Array(audioSearch.fftSize / 2);
+    const mediaStreamSource = audioSearch.audioContext.createMediaStreamSource(audioStream);
+    const analyser = audioSearch.audioContext.createAnalyser();
+    if (!analyser.getFloatTimeDomainData) { // Safari 11...
         const uint8 = new Uint8Array(2048);
-        const gftdd = function getFloatTimeDomainData(array) {
+        analyser.getFloatTimeDomainData = function getFloatTimeDomainData(array) {
             this.getByteTimeDomainData(uint8);
             const imax = array.length;
             for (let i = 0; i < imax; i++) {
                 array[i] = (uint8[i] - 128) * 0.0078125;
             }
         };
-        window.AnalyserNode.prototype.getFloatTimeDomainData = gftdd;
     }
 
-    audioSearch.sampleBuffer = new Float32Array(audioSearch.fftSize / 2);
-    const mediaStreamSource = audioSearch.audioContext.createMediaStreamSource(audioStream);
-    const analyser = audioSearch.audioContext.createAnalyser();
     analyser.fftSize = audioSearch.fftSize;
     mediaStreamSource.connect(analyser);
     audioSearch.currentAnalyser = analyser;
