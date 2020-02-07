716e367b340e55fa435efff483361f430849292e
Dan <dannycassler@gmail.com>
2020-02-07 10:49:59 -0500

    Changed if statement in pair

--- tests/moduleTests/pitch.js
+++ tests/moduleTests/pitch.js
@@ -80,10 +80,10 @@ export default function tests() {
         for (const pair of pitch_pairs) {
             const p1 = new music21.pitch.Pitch(pair[0]);
             const p2 = new music21.pitch.Pitch(pair[1]);
-            if (pair[2] === false) {
+            if (!pair[2]) {
                 assert.notDeepEqual(p1, p2, 'Not Equal');
             }
-            else if (pair[2] === true) {
+            else if (pair[2]) {
                 assert.deepEqual(p1, p2, 'Equal');
             }
                
