cuthbertLab/music21j

commit ba7843b4d51cb0428eea5ea04b438a186b6e50f7

Improve support for rendering ties in voices

src/vfShow.ts
@@ -402,7 +402,8 @@ export class Renderer {
      * @param {Stream} p - a Part or similar object
      */
     prepareTies(p: stream.Stream) {
-        const pf = <note.GeneralNote[]> Array.from(p.flat.notesAndRests);
+        // TODO: bridge voices across measures -- this won't get ties in voices across barlines
+        const pf = <note.GeneralNote[]> Array.from(p.recurse().notesAndRests);
         // console.log('newSystemsAt', this.systemBreakOffsets);
         for (let i = 0; i < pf.length - 1; i++) {
             const thisNote = pf[i];

tests/moduleTests/vfShow.ts
@@ -45,4 +45,30 @@ export default function tests() {
         renderer.render();
         assert.equal(renderer.beamGroups.length, 2);
     });
+
+    test('music21.vfShow.Renderer prepareTies in voices', assert => {
+        const v1 = new music21.stream.Voice();
+        const n0 = new music21.note.Note('F4');
+        v1.repeatAppend(n0, 2);
+        const v2 = new music21.stream.Voice();
+        const n1 = new music21.note.Note('C4');
+        n1.tie = new music21.tie.Tie('start');
+        const n2 = new music21.note.Note('C4');
+        n2.tie = new music21.tie.Tie('stop');
+        v2.append(n1);
+        v2.append(n2);
+        const p = new music21.stream.Part();
+        const m = new music21.stream.Measure();
+        m.append(new music21.clef.TrebleClef());
+        m.append(new music21.meter.TimeSignature('2/4'));
+        m.insert(0, v1);
+        m.insert(0, v2);
+        p.append(m);
+
+        const svg = p.appendNewDOM();
+        const renderer = new music21.vfShow.Renderer(p, svg);
+        renderer.preparePartlike(p);
+        assert.deepEqual(renderer.vfTies[0].first_note, n1.activeVexflowNote);
+        assert.deepEqual(renderer.vfTies[0].last_note, n2.activeVexflowNote);
+    });
 }

commit 94c5f4430852c675a063635ad4e52e46a39304b3

Fix regression in ties over system breaks Regression in ba7843b4d51cb0428eea5ea04b438a186b6e50f7.

src/vfShow.ts
@@ -416,8 +416,8 @@ export class Renderer {
             for (let sbI = 0; sbI < this.systemBreakOffsets.length; sbI++) {
                 const thisSystemBreak = this.systemBreakOffsets[sbI];
                 if (
-                    thisNote.offset < thisSystemBreak
-                    && nextNote.offset >= thisSystemBreak
+                    thisNote.getOffsetInHierarchy(p) < thisSystemBreak
+                    && nextNote.getOffsetInHierarchy(p) >= thisSystemBreak
                 ) {
                     onSameSystem = false;
                     break;

tests/moduleTests/vfShow.ts
@@ -71,4 +71,16 @@ export default function tests() {
         assert.deepEqual(renderer.vfTies[0].first_note, n1.activeVexflowNote);
         assert.deepEqual(renderer.vfTies[0].last_note, n2.activeVexflowNote);
     });
+
+    test('music21.vfShow.Renderer prepareTies across system break', assert => {
+        const p = <music21.stream.Part> music21.tinyNotation.TinyNotation('c1 d e~ e');
+        const s = new music21.stream.Score();
+        s.append(p);
+        s.renderOptions.maxSystemWidth = 20;
+
+        const svg = s.appendNewDOM();
+        const renderer = new music21.vfShow.Renderer(p, svg);
+        renderer.prepareScorelike(s);
+        assert.equal(renderer.vfTies.length, 2);
+    });
 }