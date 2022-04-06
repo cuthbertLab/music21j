d690a988771d8c1d93e70124e5b6242e838e95f8
Jacob Walls <jacobtylerwalls@gmail.com>
2022-04-05 22:27:15 -0400

    Improve test

--- tests/moduleTests/stream/iterator.ts
+++ tests/moduleTests/stream/iterator.ts
@@ -5,11 +5,23 @@ const { test } = QUnit;
 
 export default function tests() {
     test('music21.stream.iterator.OffsetIterator', assert => {
-        const n = new music21.note.Note('C#');
-        n.duration.type = 'whole';
+        const c = new music21.note.Note('C');
+        const d = new music21.note.Note('D');
+        const e = new music21.note.Note('E');
+        const f = new music21.note.Note('F');
+
         const s = new music21.stream.Stream();
-        s.repeatAppend(n, 5);
+        // C & E dyad moving to D & F
+        s.insert(0, c);
+        s.insert(1, d);
+        s.insert(0, e);
+        s.insert(1, f);
+
         const iter = new music21.stream.iterator.OffsetIterator(s);
-        assert.equal(iter.length, 5);
+        assert.equal(iter.length, 2);
+        assert.equal(
+            iter.map(x => (x[0] as music21.note.Note).pitch.name),
+            ['C', 'E', 'D', 'F']
+        );
     });
 }
