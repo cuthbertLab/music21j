4dfd2139976421924750dfa08fdd114a03e58740
Jacob Walls <jacobtylerwalls@gmail.com>
2022-04-05 22:35:19 -0400

    Add a third element to each offset

--- tests/moduleTests/stream/iterator.ts
+++ tests/moduleTests/stream/iterator.ts
@@ -9,19 +9,23 @@ export default function tests() {
         const d = new music21.note.Note('D');
         const e = new music21.note.Note('E');
         const f = new music21.note.Note('F');
+        const g = new music21.note.Note('G');
+        const a = new music21.note.Note('A');
 
         const s = new music21.stream.Stream();
-        // C & E dyad moving to D & F
+        // CEG triad moving to DFA
         s.insert(0, c);
         s.insert(1, d);
         s.insert(0, e);
         s.insert(1, f);
+        s.insert(0, g);
+        s.insert(1, a);
 
         const iter = new music21.stream.iterator.OffsetIterator(s);
         assert.equal(iter.length, 2);
         assert.equal(
             iter.map(x => (x[0] as music21.note.Note).pitch.name),
-            ['C', 'E', 'D', 'F']
+            ['C', 'E', 'G', 'D', 'F', 'A']
         );
     });
 }
