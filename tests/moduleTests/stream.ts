cuthbertLab/music21j

commit 736ef2e793b44d42f4c8d83a1fa4898299343be1

Fix chord accidentals regression

src/pitch.ts
@@ -18,6 +18,7 @@ import type * as clef from './clef';
 interface UpdateAccidentalDisplayParams {
     pitchPast?: Pitch[],
     pitchPastMeasure?: Pitch[],
+    otherSimultaneousPitches?: Pitch[],
     alteredPitches?: Pitch[],
     cautionaryPitchClass?: boolean,
     cautionaryAll?: boolean,
@@ -618,6 +619,7 @@ export class Pitch extends prebase.ProtoM21Object {
         {
             pitchPast = [],
             pitchPastMeasure = [],
+            otherSimultaneousPitches = [],
             alteredPitches = [],
             cautionaryPitchClass = true,
             cautionaryAll = false,
@@ -626,10 +628,6 @@ export class Pitch extends prebase.ProtoM21Object {
             lastNoteWasTied = false,
         }: UpdateAccidentalDisplayParams = {}
     ) {
-        // TODO: this presently deals with chords as simply a list
-        //   we might permit pitchPast to contain a list of pitches, to represent
-        //    a simultaneity?
-
         // should we display accidental if no previous accidentals have been displayed
         // i.e. if it's the first instance of an accidental after a tie
         let displayAccidentalIfNoPreviousAccidentals = false;
@@ -667,6 +665,28 @@ export class Pitch extends prebase.ProtoM21Object {
             } else {
                 return;  // exit: nothing more to do
             }
+        }
+
+        function same_step_different_pitch(p: Pitch) {
+            if (p.step !== this.step) {
+                return false;
+            }
+            return p.nameWithOctave !== this.nameWithOctave;
+        }
+
+        if (
+            cautionaryPitchClass
+            && otherSimultaneousPitches !== undefined
+            && otherSimultaneousPitches.length > 0
+            && otherSimultaneousPitches.filter(same_step_different_pitch, this).length > 0
+        ) {
+            if (acc_orig !== undefined) {
+                acc_orig.displayStatus = true;
+            } else {
+                this.accidental = new Accidental('natural');
+                this.accidental.displayStatus = true;
+            }
+            return;
         }
 
         // no pitches in past...
@@ -894,9 +914,7 @@ export class Pitch extends prebase.ProtoM21Object {
                         this.accidental = new Accidental('natural');
                     }
                     this.accidental.displayStatus = true;
-                // not exactly equivalent with https://github.com/cuthbertLab/music21/pull/1299
-                // because m21j does not track chordAttached
-                // Thus, some potential for subsequent same pitch class to lack a cautionary natural
+
                     // other cases: already natural in past usage, do not need
                     // natural again (and not in key sig)
                 } else {

tests/moduleTests/stream.ts
@@ -556,10 +556,10 @@ export default function tests() {
 
     test('music21.stream.Stream makeAccidentals augmented unison in chord', assert => {
         const m = new music21.stream.Measure();
-        const c = new music21.chord.Chord('G# G');
+        const c = new music21.chord.Chord('G G#');
         m.append(c);
         m.makeAccidentals({inPlace: true});
-        assert.ok(c.pitches[0].accidental.displayStatus);  // G comes first
+        assert.ok(c.pitches[0].accidental.displayStatus);
     });
 
     test('music21.stream.Stream makeBeams with stemDirection', assert => {

src/stream.ts
@@ -1782,6 +1782,10 @@ export class Stream extends base.Music21Object {
             alteredPitches.push(...addAlterPitches);
         }
 
+        function different_pitch(other: pitch.Pitch) {
+            return other.nameWithOctave !== this.nameWithOctave;  // this is `p`
+        }
+
         const noteIterator = returnObj.recurse().notesAndRests;
         let last_measure: Measure;
         for (const e of noteIterator) {
@@ -1818,14 +1822,15 @@ export class Stream extends base.Music21Object {
             } else if (e instanceof Chord) {
                 const chordNotes = e.notes;
                 const seenPitchNames: Set<string> = new Set();
-                pitchPast.push(...e.pitches);
                 for (const n of chordNotes) {
                     const p = n.pitch;
                     const lastNoteWasTied: boolean = tiePitchSet.has(p.nameWithOctave);
+                    const otherSimultaneousPitches = e.pitches.filter(different_pitch, p);
 
                     p.updateAccidentalDisplay({
                         pitchPast,
                         pitchPastMeasure,
+                        otherSimultaneousPitches,
                         alteredPitches,
                         cautionaryPitchClass,
                         cautionaryAll,
@@ -1842,6 +1847,7 @@ export class Stream extends base.Music21Object {
                 for (const pName of seenPitchNames) {
                     tiePitchSet.add(pName);
                 }
+                pitchPast.push(...e.pitches);
             } else {
                 tiePitchSet.clear();
             }

commit 84ef2b227a21e40fa8a6e1ce09509b03bf06ac47

Fix makeAccidentals regression involving cautionaryPitchClass and chords
Regression in 736ef2e793b44d42f4c8d83a1fa4898299343be1.

src/pitch.ts
@@ -671,7 +671,7 @@ export class Pitch extends prebase.ProtoM21Object {
             if (p.step !== this.step) {
                 return false;
             }
-            return p.nameWithOctave !== this.nameWithOctave;
+            return p.pitchClass !== this.pitchClass;
         }
 
         if (

tests/moduleTests/stream.ts
@@ -562,6 +562,14 @@ export default function tests() {
         assert.ok(c.pitches[0].accidental.displayStatus);
     });
 
+    test('music21.stream.Stream makeAccidentals perfect octave in chord', assert => {
+        const m = new music21.stream.Measure();
+        const c = new music21.chord.Chord('G4 G5');
+        m.append(c);
+        m.makeAccidentals({inPlace: true});
+        assert.notOk(c.pitches[0].accidental?.displayStatus);
+    });
+
     test('music21.stream.Stream makeBeams with stemDirection', assert => {
         const n1 = new music21.note.Note('C5', 0.5);
         n1.stemDirection = 'up';